a3553d2b6531079300ad3075a5c974bd9562fc13
sbhar <sbharti924@gmail.com>
2023-03-15 18:04:35 -0500

    added homepage

---
+++ src/test/java/com/tests/HomePage.java
@@ -0,0 +1,13 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+public class HomePage {
+
+    @Test
+    public void homepage(){
+
+        System.out.println("im in homepage");
+    }
+
+}
